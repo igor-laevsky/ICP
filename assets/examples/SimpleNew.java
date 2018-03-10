c0206e8eb2386e4c3dad6218f11a31709fa1864a
Igor Laevsky <igmyrj@gmail.com>
2018-03-10 14:29:44 +0300

    Add new example

---
+++ assets/examples/SimpleNew.java
@@ -0,0 +1,16 @@
+public class SimpleNew {
+  public static int ca = 15;
+  public static int cb = 20;
+
+  public int a, b;
+
+  SimpleNew(int aa, int bb) {
+    a = aa;
+    b = bb;
+  }
+
+  public static int main(String[] args) {
+    SimpleNew a = new SimpleNew(5, 10);
+    return a.a + a.b + ca + cb;
+  }
+}
